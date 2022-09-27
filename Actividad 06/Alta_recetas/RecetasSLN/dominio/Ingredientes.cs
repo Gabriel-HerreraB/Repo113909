0a7a61dcdf86f0fad16a78b96faa63f9254fe4ea
Gabriel-HerreraB <gabriel.herrerabaron@gmail.com>
2022-09-27 17:36:25 -0300

    Actividad 06

---
+++ Actividad 06/Alta_recetas/RecetasSLN/dominio/Ingredientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.dominio
+{
+    internal class Ingredientes
+    {
+        public int IdIngrediente { get; set; }
+        public string Nombre { get; set; }
+        public double Unidad { get; set; }
+
+
+    }
+}
---
+++ Actividad 06/Alta_recetas/RecetasSLN/presentación/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace RecetasSLN.presentación
+{
+    partial class FrmAltaRecetas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTipoR = new System.Windows.Forms.ComboBox();
+            this.lblNReceta = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.dgvAlta = new System.Windows.Forms.DataGridView();
+            this.Ingrediente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Acciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtChef = new System.Windows.Forms.TextBox();
+            this.cboIngredientes = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboTipoR
+            // 
+            this.cboTipoR.FormattingEnabled = true;
+            this.cboTipoR.Location = new System.Drawing.Point(87, 103);
+            this.cboTipoR.Name = "cboTipoR";
+            this.cboTipoR.Size = new System.Drawing.Size(121, 21);
+            this.cboTipoR.TabIndex = 0;
+            // 
+            // lblNReceta
+            // 
+            this.lblNReceta.AutoSize = true;
+            this.lblNReceta.Location = new System.Drawing.Point(55, 9);
+            this.lblNReceta.Name = "lblNReceta";
+            this.lblNReceta.Size = new System.Drawing.Size(65, 13);
+            this.lblNReceta.TabIndex = 1;
+            this.lblNReceta.Text = "RECETA N°";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(87, 36);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(315, 159);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // nudCantidad
+            // 
+            this.nudCantidad.Location = new System.Drawing.Point(178, 162);
+            this.nudCantidad.Name = "nudCantidad";
+            this.nudCantidad.Size = new System.Drawing.Size(120, 20);
+            this.nudCantidad.TabIndex = 4;
+            // 
+            // dgvAlta
+            // 
+            this.dgvAlta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ingrediente,
+            this.Cantidad,
+            this.Acciones});
+            this.dgvAlta.Location = new System.Drawing.Point(13, 197);
+            this.dgvAlta.Name = "dgvAlta";
+            this.dgvAlta.Size = new System.Drawing.Size(377, 150);
+            this.dgvAlta.TabIndex = 5;
+            // 
+            // Ingrediente
+            // 
+            this.Ingrediente.HeaderText = "Ingrediente";
+            this.Ingrediente.Name = "Ingrediente";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Acciones
+            // 
+            this.Acciones.HeaderText = "Acciones";
+            this.Acciones.Name = "Acciones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chef";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tipo Receta ";
+            // 
+            // txtChef
+            // 
+            this.txtChef.Location = new System.Drawing.Point(87, 63);
+            this.txtChef.Name = "txtChef";
+            this.txtChef.Size = new System.Drawing.Size(100, 20);
+            this.txtChef.TabIndex = 9;
+            // 
+            // cboIngredientes
+            // 
+            this.cboIngredientes.FormattingEnabled = true;
+            this.cboIngredientes.Location = new System.Drawing.Point(13, 161);
+            this.cboIngredientes.Name = "cboIngredientes";
+            this.cboIngredientes.Size = new System.Drawing.Size(121, 21);
+            this.cboIngredientes.TabIndex = 10;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(15, 364);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 11;
+            this.btnAceptar.Text = "button2";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(111, 364);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "button3";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FrmAltaRecetas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 392);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cboIngredientes);
+            this.Controls.Add(this.txtChef);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvAlta);
+            this.Controls.Add(this.nudCantidad);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNReceta);
+            this.Controls.Add(this.cboTipoR);
+            this.Name = "FrmAltaRecetas";
+            this.Text = "Alta Recetas";
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboTipoR;
+        private System.Windows.Forms.Label lblNReceta;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.DataGridView dgvAlta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ingrediente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Acciones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtChef;
+        private System.Windows.Forms.ComboBox cboIngredientes;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
